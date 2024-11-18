5a4ebe1862a5d18de09eb1dd05ec51fb24aeb382
LucioAJ <lalmaraz@students.kennesaw.edu>
2024-11-18 15:50:42 -0500

    Change 3
    While I was stuck on what to do of this code (change 1-2) that I was a bit to quit but continue to make some changes. So I started to use some coding variables where to make the game function. Such as, the "check_answer" function makes the players guess at the remaining attempt. Compare input to answer by displaying a winning message towards the player by hiding the "check" button. Finally, at line 26-27 I started to make sure that non-numbers input that does not make the program crash out as my previous (beta version) and let the user to try again. Now, the reason why I added these changes because for starters I do not want this main code to crash out as my previous code (beta version). Additionally, the coding structure summarize the errors correctly, making sure that the user playing the game has a non-crash experience.

--- (Main) Guess the number game - Lucio AJ.py
+++ (Main) Guess the number game - Lucio AJ.py
@@ -3,3 +3,25 @@ import random
 
 attempts = 10
 answer = random.randint(1,100)
+
+def check_answer():
+    global attmepts
+
+    try:
+       
+        attempts -= 1
+
+        guess = int(entry_window.get())
+
+        if answer == guess:
+            text.set("Congratulations! YOU WON!")
+            btn_check.pack_forget()
+        elif attempts == 0:
+            text.set("You ran out of attempts! Try again next time!")
+            btn_check.pack_forget()
+        elif guess < answer:
+            text.set(f"Incorrect! you have {attempts} remaining - Go a bit higher!")
+        elif guess > answer:
+            text.set(f"Incorrect! you have {attempts} remaining - Go a bit lower!")
+    except ValueError:
+        text.set("Please enter a valid mumber only (ex. 0, 1, 2, 3)")
