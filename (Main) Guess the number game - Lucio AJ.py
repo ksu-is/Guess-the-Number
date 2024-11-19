df643c283278231b022c2ab4bda88f6095034aec
LucioAJ <lalmaraz@students.kennesaw.edu>
2024-11-18 22:06:54 -0500

    Change 6
    While almost wrapping up the code,  I started to put the final code which is "root.mainloop()". This keeps the Tkinter window running, allowing the user interact with the game. The even loop is mandatory to maintain the GUI's responsive

--- (Main) Guess the number game - Lucio AJ.py
+++ (Main) Guess the number game - Lucio AJ.py
@@ -1,16 +1,18 @@
-from tkinter import*
+from tkinter import *
 import random
 
+
 attempts = 10
-answer = random.randint(1,100)
+answer = random.randint(1, 99)
 
 def check_answer():
-    global attmepts
+    global attempts
 
     try:
-       
+        
         attempts -= 1
 
+        
         guess = int(entry_window.get())
 
         if answer == guess:
@@ -20,27 +22,35 @@ def check_answer():
             text.set("You ran out of attempts! Try again next time!")
             btn_check.pack_forget()
         elif guess < answer:
-            text.set(f"Incorrect! you have {attempts} remaining - Go a bit higher!")
+            text.set(f"Incorrect! You have {attempts} attempts remaining - Go a bit higher!")
         elif guess > answer:
-            text.set(f"Incorrect! you have {attempts} remaining - Go a bit lower!")
+            text.set(f"Incorrect! You have {attempts} attempts remaining - Go a bit lower!")
     except ValueError:
-        text.set("Please enter a valid mumber only (ex. 0, 1, 2, 3)")
+        
+        text.set("Please enter a valid number!")
+
+
 root = Tk()
 root.title("Guess that Number!")
 root.geometry('500x158')
 
-Label(root, text="Guess the number between 1 through 100").pack()
+
+Label(root, text="Guess the number between 1 and 99").pack()
 
 entry_window = Entry(root, width=40, borderwidth=4)
 entry_window.pack()
 
 btn_check = Button(root, text="Check", command=check_answer)
-btn_check.pack
+btn_check.pack()
 
 btn_quit = Button(root, text="Quit", command=root.destroy)
 btn_quit.pack()
 
+
 text = StringVar()
-text.set("You have 10 attempts remaining! Good Luck and don't give up!")
+text.set("You have 10 attempts remaining! Good luck!")
 guess_attempts = Label(root, textvariable=text)
 guess_attempts.pack()
+
+
+root.mainloop()
