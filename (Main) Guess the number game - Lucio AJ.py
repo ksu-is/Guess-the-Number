643ddcf5e35a53f99bfeb73b5f0d8ee6d2a4304b
LucioAJ <lalmaraz@students.kennesaw.edu>
2024-11-18 21:39:26 -0500

    Change 5
    After an hour, thinking what to be putting on the game, I starting to find some inspiration of what a game needs. Now, I found that the mid-beginning lines (line 16-25), there were some missing coding that I need to fix in order to stop the error. So, I proceeded to add some coding function on line (37-46). A quit and check button weather the player want to quit or check their guess. Finally, I added some hints because a game need hints on a guessing game. Now, the reason why I added these code functions in my guessing game is to have a layout where buttons clearly function, a text field for input variables, and labels for adding details to ensure an interface.

--- (Main) Guess the number game - Lucio AJ.py
+++ (Main) Guess the number game - Lucio AJ.py
@@ -33,3 +33,14 @@ Label(root, text="Guess the number between 1 through 100").pack()
 
 entry_window = Entry(root, width=40, borderwidth=4)
 entry_window.pack()
+
+btn_check = Button(root, text="Check", command=check_answer)
+btn_check.pack
+
+btn_quit = Button(root, text="Quit", command=root.destroy)
+btn_quit.pack()
+
+text = StringVar()
+text.set("You have 10 attempts remaining! Good Luck and don't give up!")
+guess_attempts = Label(root, textvariable=text)
+guess_attempts.pack()
